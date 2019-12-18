59b22a903c09a13e3d36543ac10254e3b5044ccf
Babechick13 <jessicanatovich@yahoo.com>
2019-12-18 15:33:22 -0500

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,6 +1,8 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+"SELECT SUM(tardies) AS total_tardies FROM students;"
+end 
 
 def highest_student_gpa
+    "SELECT grade, COUNT(*) FROM students GROUP BY grade;"
 end
 
 def lowest_student_gpa
@@ -13,4 +15,6 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
+    "SELECT AVG(gpa) FROM students WHERE grade==9;"
 end
+
